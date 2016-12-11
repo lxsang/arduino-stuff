3a3aa4280485e848ea9647f1d10a13fd58169d1b
lxsang <xsang.le@gmail.com>
2016-12-11 01:16:27 +0100

    remove files

--- sketches/shrc04/shrc04.d
+++
@@ -1,6 +0,0 @@
-shrc04.o: shrc04.cpp ../include/Arduino.h ../include/binary.h \
- ../include/WCharacter.h ../include/WString.h ../include/HardwareSerial.h \
- ../include/Stream.h ../include/Print.h ../include/Printable.h \
- ../include/USBAPI.h ../include/Arduino.h \
- ../include/eightanaloginputs/pins_arduino.h \
- ../include/eightanaloginputs/../standard/pins_arduino.h
