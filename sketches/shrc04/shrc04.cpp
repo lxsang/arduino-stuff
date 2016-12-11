1e36e0ed1e99d9e10dba66f65f1b18e38fca3ef2
lxsang <xsang.le@gmail.com>
2016-12-11 01:13:39 +0100

    add files

---
+++ sketches/shrc04/shrc04.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+
+#define trigPin 12
+#define echoPin 13
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(50);
+}
